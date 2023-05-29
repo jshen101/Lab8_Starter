d8df297e9fd0dedddfeb05cfd0aeaa3cf8031870
Leica <leica.shen@gmail.com>
2023-05-28 21:30:28 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Lab 8 - Starter
+
+### 1  Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+- Within a Github action that runs whenever code is pushed 
+
+I would fit the automated tests in my Recipe project development within a Github action that runs whenever code is pushed. In this way, everytime I make some changes and push, the test will be run automatically. This prevents the errors that I didn't notice when I changed my code, so when I am planning to merge the error code to github, the test will alert me about it. 
+
+### 2 Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+- No
+
+Unit test can ensures that the functions have correct outputs that are working as required. End-to-end test ensures the complete workflows work as required. Thus, I would use unit test to check if a function is returning the correct output instead of end to end test. 
+
+### 3 Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+- No 
+
+I would not use a unit test to test the "message" feature of a messaging application, because unit test is good for testing whether a function could work as expected, it does not involve how individual components interact with each other on feature level. I would choose End-to-end test for the "message" feature. 
+
+### 4 Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+- Yes 
+
+I would use a unit test to test the "max message length" feature of a messaging application, because "max message length" is not likely to be dependent from other components and checking whether the input length has exceed a certain limit could be done by a function, and unit tests are suitable for testing functions' behaviors.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('isPhoneNumber valid', () => {
+    expect(functions.isPhoneNumber('123 456-7890')).toBe(true);
+});
+
+test('isPhoneNumber invalid', () => {
+    expect(functions.isPhoneNumber('12')).toBe(false);
+});
+
+test('isPhoneNumber valid', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('isPhoneNumber invalid', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('isEmail valid', () => {
+    expect(functions.isEmail('jis033@ucsd.edu')).toBe(true);
+});
+
+test('isEmail invalid', () => {
+    expect(functions.isEmail('.edu')).toBe(false);
+});
+
+test('isEmail valid', () => {
+    expect(functions.isEmail('123@ucsd.edu')).toBe(true);
+});
+
+test('isEmail invalid', () => {
+    expect(functions.isEmail('jis033@.edu')).toBe(false);
+});
+
+test('isStrongPassword valid', () => {
+    expect(functions.isStrongPassword('a1234567890123')).toBe(true);
+});
+
+test('isStrongPassword invalid', () => {
+    expect(functions.isStrongPassword('abc123456789012345')).toBe(false);
+});
+
+test('isStrongPassword valid', () => {
+    expect(functions.isStrongPassword('a123456789012')).toBe(true);
+});
+
+test('isStrongPassword invalid', () => {
+    expect(functions.isStrongPassword('1234567890')).toBe(false);
+});
+
+test('isDate valid', () => {
+    expect(functions.isDate('1/1/2023')).toBe(true);
+});
+
+test('isDate invalid', () => {
+    expect(functions.isDate('11/23')).toBe(false);
+});
+
+test('isDate valid', () => {
+    expect(functions.isDate('11/11/2023')).toBe(true);
+});
+
+test('isDate invalid', () => {
+    expect(functions.isDate('123/23')).toBe(false);
+});
+
+test('isHexColor valid', () => {
+    expect(functions.isHexColor('#ffffff')).toBe(true);
+});
+
+test('isHexColor invalid', () => {
+    expect(functions.isHexColor('12z')).toBe(false);
+});
+
+test('isHexColor valid', () => {
+    expect(functions.isHexColor('ab9')).toBe(true);
+});
+
+test('isHexColor invalid', () => {
+    expect(functions.isHexColor('12345678')).toBe(false);
+});
